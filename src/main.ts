2552eab52ae3c7171b788e737c0cec5766d47bac
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-03-24 13:12:12 +0600

    add: Вынес Kawarimi в класс.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ interface IMocksDictionary {
 	[key: string]: any;
 };
 
+interface Kawarimi {
+	instance: AxiosInstance
+}
+
 const mocks: IMocksDictionary = {
 	'[get]:[http://localhost:3001/type-viewing]': {
 		data: [
@@ -33,20 +37,28 @@ const mocks: IMocksDictionary = {
 	},
 };
 
-export function kawarimiMocker(instance: AxiosInstance): void {
-	instance.interceptors.response.use(async (response: AxiosResponse) => {
-		let data: AxiosResponse = JSON.parse(JSON.stringify(response));
-		const key: string = `[${response.config.method}]:[${response.config.baseURL}${response.config.url}]`;
+class Kawarimi {
+	constructor(instance: AxiosInstance) {
+		this.instance = instance;
+	}
 
-		if (mocks[key]) {
-			data = {
-				...data,
-				...mocks[key]
+	mocker() {
+		this.instance.interceptors.response.use(async (response: AxiosResponse) => {
+			let data: AxiosResponse = JSON.parse(JSON.stringify(response));
+			const key: string = `[${response.config.method}]:[${response.config.baseURL}${response.config.url}]`;
+
+			if (mocks[key]) {
+				data = {
+					...data,
+					...mocks[key]
+				}
 			}
-		}
 
-		return data;
-	}, (error: Error) => {
-		return Promise.reject(error);
-	});
-}
+			return data;
+		}, (error: Error) => {
+			return Promise.reject(error);
+		});
+	}
+};
+
+export default Kawarimi;
