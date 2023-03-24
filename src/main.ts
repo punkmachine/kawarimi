8980ed02e9908351f1b32461431fa8eedbf6a5c9
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-03-24 15:25:21 +0600

    upd: выделил моки в функции для экономии памяти.

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,39 @@
 import { InternalAxiosRequestConfig } from '.';
 
 interface IMocksDictionary {
-	[key: string]: any;
+	[key: string]: Function;
 };
 
 const mocks: IMocksDictionary = {
-	'[get]:[http://localhost:3001/type-viewing]': {
-		data: [
-			"movie",
-			"cartoon",
-			"series",
-			"animated_series",
-			"anime",
-			"anime_series"
-		]
+	'[get]:[http://localhost:3001/type-viewing]': () => {
+		return {
+			data: [
+				"movie",
+				"cartoon",
+				"series",
+				"animated_series",
+				"anime",
+				"anime_series"
+			]
+		}
 	},
-	'[get]:[http://localhost:3001/watch-list]': {
-		data: [
-			{
-				"id": "1676850018403",
-				"name": "Абстракция",
-				"is-franchise": false,
-				"type-viewing": "series"
-			},
-			{
-				"id": "1677390343862",
-				"name": "Достать ножи: Стеклянная луковица",
-				"is-franchise": false,
-				"type-viewing": "movie"
-			},
-		]
+	'[get]:[http://localhost:3001/watch-list]': () => {
+		return {
+			data: [
+				{
+					"id": "1676850018403",
+					"name": "Абстракция",
+					"is-franchise": false,
+					"type-viewing": "series"
+				},
+				{
+					"id": "1677390343862",
+					"name": "Достать ножи: Стеклянная луковица",
+					"is-franchise": false,
+					"type-viewing": "movie"
+				},
+			]
+		}
 	},
 };
 
@@ -39,7 +43,7 @@ export function mockingData(config: InternalAxiosRequestConfig) {
 		let data;
 
 		if (mocks[key]) {
-			data = mocks[key];
+			data = mocks[key]();
 		}
 
 		resolve(data);
