ef3895a0cd986b5373662fcc8c0dd7f84b6a9da9
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-03-25 11:55:54 +0600

    test: пуш тестовых данных для моделек.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,56 @@
 import { InternalAxiosRequestConfig } from '.';
 
-interface IMocksDictionary {
+interface IMocks {
 	[key: string]: Function;
 };
 
-const mocks: IMocksDictionary = {
+interface IModels {
+	[key: string]: Function;
+};
+
+const enumsList = {
+	'status-orders': [
+		1, 2, 3, 4, 5, 6
+	]
+};
+
+const modelsExample: IModels = {
+	'[get]:[http://localhost:3001/orders]': () => {
+		return {
+			type: 'array', // тип ответа: объект, массив, строка, число.
+			properties: {
+				id: {
+					type: 'int',
+					required: true,
+					datatype: 'id',
+				},
+				courierName: {
+					type: 'string',
+					nullable: true,
+					datatype: 'name'
+				},
+				address: {
+					type: 'link',
+					required: true,
+					datatype: 'address',
+					link: 'points' // ссылка на условную модельку поинтов
+				},
+				status: {
+					type: 'enum',
+					required: true,
+					datatype: 'enum',
+					linkEnum: 'status-orders' // ссылка на схему енама где-то в схемах
+				}
+			},
+			link: { // подразумевается, что в типах будет указан ссылочный тип из сваггера
+				name: 'orders'
+			}
+		}
+	}
+};
+
+// Данные, которые мы будем генерировать из моделей:
+const mocks: IMocks = {
 	'[get]:[http://localhost:3001/type-viewing]': () => {
 		return {
 			data: [
@@ -37,8 +83,21 @@ const mocks: IMocksDictionary = {
 	},
 };
 
+function getModel(config) {
+	const key: string = `[${config.method}]:[${config.baseURL}${config.url}]`;
+
+	return modelsExample[key];
+}
+
+function generateData(config) {
+	/*
+		count - количество элементов в массиве.
+		properties - объект, внутри которого будут данные свойств, которые должны быть заданы статично, а не генератором
+	*/
+}
+
 export function mockingData(config: InternalAxiosRequestConfig) {
-    return new Promise(resolve => {
+	return new Promise(resolve => {
 		const key: string = `[${config.method}]:[${config.baseURL}${config.url}]`;
 		let data;
 
